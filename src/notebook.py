3326d070fd0355656b8aee93f40a17d51fb9b8a2
MIGUEL456XDXD <mynameismiguel777@gmail.com>
2026-02-16 19:33:34 -0500

    edit 79a2aad Update README to reflect correct file paths and enhance AI usage advice

--- src/notebook.py
+++ src/notebook.py
@@ -1,1 +1,9 @@
-# TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+class Notebook:
+    def __init__(self):
+        self.notas = []
+
+    def add_note(self, title: str, text: str, importance: str) -> int:
+        new_code = len(self.notas) + 1
+        note = Note(new_code, title, text, importance)
+        self.notas.append(note)
+        return new_code
